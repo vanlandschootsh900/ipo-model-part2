e8e58f179a9cf88c465b48d84f4c781f1bdeaf48
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2024-09-18 10:46:28 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#Shay Vanlandschoot
+#9/18/24
+#IPO intro
+
+num1 = int(input('Enter a number:  '))
+num2 = int(input('Enter another number: '))
+#num3 = int(input('Enter another number: '))
+
+total = num1 + num2
+#total = num1 * num2 ** num3 
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+#print(f'Third number entered was: {num3}')
+print()
+print(f'The sum of {num1} and {num2} is: {total}')
+#print(f'The sum of {num1} and {num2} and {num3} is: {total}')
